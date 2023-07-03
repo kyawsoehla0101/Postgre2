72877cf91857bf957313691a5ca051e10c4c1379
Than Than Soe <thanthansoe15151@gmail.com>
2023-07-03 17:04:55 +0630

    first comment

---
+++ Post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.IntegerField()
+    
+
+    def __str__(self):
+        return self.name
