3982c636f44d228dd7410da36fc86a82dd7d9d00
Than Than Soe <thanthansoe15151@gmail.com>
2023-07-04 15:20:48 +0630

    added db.sqlite3

--- railway_django/settings.py
+++ railway_django/settings.py
@@ -65,15 +65,11 @@ WSGI_APPLICATION = 'railway_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'mLzJUrPpOYBhlo3l9Z5Z',
-        'HOST': 'containers-us-west-204.railway.app',
-        'PORT': '6571',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
